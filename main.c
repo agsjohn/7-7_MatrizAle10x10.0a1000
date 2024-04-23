6395b2a6fae289e25d87621760eb2a20b187b68a
João Vitor <136394408+agsjohn@users.noreply.github.com>
2024-04-23 10:16:32 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ALE 1001
+#define MAX 10
+
+main(){
+  int c, l, l1, c1, m[MAX][MAX], cont;
+  srand(time(NULL));
+  for(l = 0; l < MAX; l++){
+    for(c = 0; c < MAX; c++){
+      do{
+        do{
+          m[l][c] = rand() % ALE;
+        }while(m[l][c] % 2 != 0);
+        cont = 0;
+        for(l1 = 0; l1 <= l; l1++){
+          if(l == l1){
+            for(c1 = 0; c1 < c; c1++){
+              if(m[l][c] == m[l1][c1]){
+                cont++;
+              }
+            }
+          }
+          else{
+            for(c1 = 0; c1 < MAX; c1++){
+              if(m[l][c] == m[l1][c1]){
+                cont++;
+              }
+            }
+          }
+        }
+      }while(cont != 0);
+    }
+  }
+  printf("\nMatriz: \n");
+  for (l = 0; l < MAX; l++) {
+    for (c = 0; c < MAX; c++) {
+     if (c < MAX - 1) {
+        printf("%i - ", m[l][c]);
+      }
+      else{
+        printf("%i\n", m[l][c]);
+      }
+    }
+  }
+  getchar();
+}
